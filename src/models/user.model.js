90e20afdee4d8bf5595b9432eff49567afd74a61
Abhinav Srivastava <abhin.07av@gmail.com>
2025-06-03 14:41:41 +0530

    added models and Tokens

---
+++ src/models/user.model.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String
+  },
+  password: { 
+    type: String,
+    required: [true, " Password is required"]
+ },
+ refreshToken: {
+    type: String   
+ }
+ }, {timestamps: true});
+
+ userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next();
+
+    this.password= await bcrypt.hash(this.password, 10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+userSchema.methods.generateRefreshToken =function (){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+module.exports = mongoose.model('User', userSchema);
