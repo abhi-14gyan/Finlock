c3cb30fdbcaaf2412fbec80d7a784b861cc155e2
abhi_14gyan <abhi.14gyan@gmail.com>
2025-06-04 11:11:09 +0530

    Added oAuth for google login

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,64 +1,74 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
+  googleId: {
+    type: String,
+    default: null, // for users authenticated via Google
+  },
   email: {
     type: String,
     unique: true,
     required: true
   },
-  name: {
+  username: {
     type: String,
     required: true
   },
   imageUrl: {
-    type: String
+    type: String,
+    default: "https://res.cloudinary.com/demo/image/upload/v1717398651/default-avatar.png"
   },
   password: { 
     type: String,
-    required: [true, " Password is required"]
- },
- refreshToken: {
-    type: String   
- }
- }, {timestamps: true});
+    required: function () {
+      return !this.googleId; // password required only if not using Google
+    },
+  },
+  refreshToken: {
+    type: String
+  }
+}, { timestamps: true });
 
- userSchema.pre("save", async function (next){
-    if(!this.isModified("password")) return next();
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
-    this.password= await bcrypt.hash(this.password, 10)
-    next();
-})
+// Compare password
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password,this.password);
-}
+// Generate access token
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  );
+};
 
-userSchema.methods.generateAccessToken = function (){
-    return jwt.sign(
-        {
-            _id:this._id,
-            email:this.email,
-            username: this.username
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-        }
-
-    )
-}
-
-userSchema.methods.generateRefreshToken =function (){
-    return jwt.sign(
-        {
-            _id:this._id,
-        },
-        process.env.REFRESH_TOKEN_SECRET,
-        {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
-        }
-    )
-}
-
+// Generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  );
+};
 
 module.exports = mongoose.model('User', userSchema);
---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { generateAccessAndRefreshToken } = require("../controllers/auth.controller");
+
+// Step 1: Google login
+router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+// Step 2: Google callback
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login", session: false }),
+  async (req, res) => {
+    const user = req.user;
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+
+    // Set cookies and redirect to frontend
+    res.cookie("accessToken", accessToken, { httpOnly: true, secure: true });
+    res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: true });
+
+    res.redirect(`${process.env.FRONTEND_URL}/login/success?accessToken=${accessToken}`);
+  }
+);
+
+module.exports = router;
