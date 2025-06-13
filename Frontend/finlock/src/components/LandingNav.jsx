70981d55f65ea20b6ab845d3ec0e3dad6a14834d
Abhinav Srivastava <abhin.07av@gmail.com>
2025-06-13 15:49:56 +0530

    frontend bug fixes

--- Frontend/finlock/src/components/LandingNav.jsx
+++ Frontend/finlock/src/components/LandingNav.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { useTheme } from "../context/ThemeContext";
 import logo from "../assets/Finlocklogo.png"; // Ensure this path is correct
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 export default function LandingNav() {
-  const { theme, toggleTheme } = useTheme();
   const { user, checkingAuth } = useAuth();
   const navigate = useNavigate();
   return (
-    <nav className="w-full px-6 py-4 bg-gray-300 dark:bg-gray-900 shadow-md dark:shadow-lg transition-colors duration-300">
+    <nav className="w-full px-6 py-4 bg-gray-900 shadow-md dark:shadow-lg transition-colors duration-300">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Left Side: Logo + Name */}
         <div onClick={()=>navigate("/")} className="flex items-center space-x-3 hover:scale-105 transition-transform cursor-pointer">
           <img src={logo} alt="Finlock Logo" className="h-10 w-10" />
-          <span className="text-2xl font-bold text-black dark:text-white" >
+          <span className="text-2xl font-bold text-white" >
             Finlock
           </span>
         </div>
@@ -22,13 +20,13 @@ export default function LandingNav() {
         <div className="flex items-center space-x-6">
           <a
             href="/features"
-            className="text-gray-800 dark:text-gray-200 hover:underline"
+            className="text-gray-200 hover:underline"
           >
             Features
           </a>
           <a
             href="/contact"
-            className="text-gray-800 dark:text-gray-200 hover:underline"
+            className="text-gray-200 hover:underline"
           >
             Contact
           </a>
@@ -38,13 +36,6 @@ export default function LandingNav() {
           >
             {(user? "Dashboard" : "Login")}
           </a>
-          <button
-            onClick={toggleTheme}
-            className="text-2xl text-black dark:text-white focus:outline-none"
-            aria-label="Toggle Theme"
-          >
-            {theme === "dark" ? "🌞" : "🌙"}
-          </button>
         </div>
       </div>
     </nav>
