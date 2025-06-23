2c701308a5175267ce6efd1be85111b842a35c3f
Abhinav Srivastava <abhin.07av@gmail.com>
2025-06-23 12:48:16 +0530

    nodemialer added

--- backend/src/actions/sendEmail.js
+++ backend/src/actions/sendEmail.js
@@ -21,3 +21,38 @@ const sendEmail = async function ({ to, subject, html }) {
 };
 
 module.exports = { sendEmail };
+
+
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async function ({ to, subject, html }) {
+//   // Create the transporter
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.GMAIL_USER,      // your Gmail address
+//       pass: process.env.GMAIL_PASS       // app password (not your real password)
+//     },
+//   });
+
+//   // Email content
+//   const mailOptions = {
+//     from: `Finlock <${process.env.GMAIL_USER}>`,  // From Finlock (custom name)
+//     to,
+//     subject,
+//     html,
+//   };
+
+//   // Send the email
+//   try {
+//     console.log("📧 Sending email to:", to);
+//     const info = await transporter.sendMail(mailOptions);
+//     console.log("✅ Email sent:", info.response);
+//     return { success: true, data: info };
+//   } catch (error) {
+//     console.error("❌ Failed to send email:", error);
+//     return { success: false, error };
+//   }
+// };
+
+// module.exports = { sendEmail };
